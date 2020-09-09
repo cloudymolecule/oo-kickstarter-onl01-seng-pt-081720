a855ec2b0e2d5514fdc63f5847ed37b16f9f9a2a
Dario Carlino <baires82@gmail.com>
2020-09-09 20:26:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,6 @@
 require "pry"
 class Backer
   attr_reader :name, :backed_projects
-  attr_accessor :add_backer
 
   def initialize(name)
     @name = name
@@ -10,6 +9,6 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    Project.add_backer(self)
+    #Project.add_backer(self)
   end
 end
